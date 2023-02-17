9af0352254f60862132f3fb6094cea6f197ca22b
codesamuraii <remi@heneault.fr>
2023-02-17 12:59:55 +0100

    Implementing methods on conversation

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ pub enum ConvState {
     Closed,
 }
 
-pub struct Client<Copy> {
+pub struct Client {
     pub id: Vec<u8>,
     pub stream: TcpStream,
 }
@@ -16,4 +16,23 @@ pub struct Conversation {
     pub state: ConvState,
     pub client1: Client,
     pub client2: Option<Client>,
+}
+
+impl Conversation {
+    pub fn new(client: Client) -> Conversation {
+        Conversation {
+            state: ConvState::Waiting,
+            client1: client,
+            client2: None,
+        }
+    }
+
+    pub fn add_client(&mut self, client: Client) {
+        self.client2 = Some(client);
+        self.state = ConvState::Going;
+    }
+
+    pub fn get_id(&self) -> &Vec<u8> {
+        &self.client1.id
+    }
 }
