053f3c0fd4f9398ad9a9a3ccfa350e7967183fd3
codeSamuraii <remi@heneault.fr>
2023-02-25 20:40:18 +0100

    Switch to a more object oriented approach

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::io::{Read, Error};
 use std::net::{TcpStream};
 
 pub enum ConvState {
@@ -11,16 +12,33 @@ pub struct Client {
     pub stream: TcpStream,
 }
 
-pub struct Conversation {
+impl Client {
+    pub fn from_stream(mut stream: TcpStream) -> Result<Client, Error> {
+        let mut data = String::with_capacity(16);
+        match stream.read_to_string(&mut data) {
+            Ok(_) => { println!("New client for ID {:?}", data); },
+            Err(e) => { println!("Error: {}", e); return Err(e); }
+        }
+
+        let client = Client {
+            conv_id: data,
+            stream: stream,
+        };
+
+        return Ok(client)
+    }
+}
+
+pub struct Session {
     pub id: String,
     pub state: ConvState,
     pub client1: Client,
     pub client2: Option<Client>,
 }
 
-impl Conversation {
-    pub fn new(client: Client) -> Conversation {
-        Conversation {
+impl Session {
+    pub fn new(client: Client) -> Session {
+        Session {
             id: client.conv_id.clone(),
             state: ConvState::Waiting,
             client1: client,
@@ -31,5 +49,33 @@ impl Conversation {
     pub fn add_client(&mut self, client: Client) {
         self.client2 = Some(client);
         self.state = ConvState::Going;
+    }
+}
+
+pub struct SessionManager {
+    pub sessions: Vec<Session>
+}
+
+impl SessionManager {
+    pub fn initialize() -> SessionManager {
+        SessionManager {
+            sessions: Vec::new()
+        }
+    }
+
+    pub fn add_client(&mut self, client: Client) {
+        // Look for an existing matching session
+        for session in self.sessions.iter_mut() {
+            if session.id == client.conv_id {
+                println!("Adding client to session: {:?}", session.id);
+                session.add_client(client);
+                return;
+            }
+        }
+
+        // Create one if it doesn't exist
+        let session = Session::new(client);
+        println!("Creating session {:?}", session.id);
+        self.sessions.push(session);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,17 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use converse::{Client, Conversation, ConvState};
-
-
-fn get_client(mut stream: TcpStream) -> Result<Client, std::io::Error> {
-    let mut data = String::with_capacity(16);
-    match stream.read_to_string(&mut data) {
-        Ok(_) => { println!("Client ID: {:?}", data); },
-        Err(e) => { println!("Error: {}", e); return Err(e); }
-    }
-
-    let client = Client {
-        conv_id: data,
-        stream: stream,
-    };
-
-    return Ok(client);
-}
-
-
-fn add_to_conversation(client: Client, conversations: &mut Vec<Conversation>) {
-    for conv in conversations.iter_mut() {
-        if conv.id == client.conv_id {
-            println!("Adding client to conversation: {:?}", conv.id);
-            conv.add_client(client);
-            return;
-        }
-    }
-
-    let conv = Conversation::new(client);
-    println!("Created new conversation: {:?}", conv.id);
-    conversations.push(conv);
-}
+use converse::{Client, SessionManager};
 
 
 fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:3443")?;
-    let mut conversations: Vec<Conversation> = Vec::new();
+    let mut session_manager = SessionManager::initialize();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                let client = get_client(stream).unwrap();
-                add_to_conversation(client, &mut conversations)
+                let client = Client::from_stream(stream).unwrap();
+                session_manager.add_client(client)
             }
             Err(e) => { println!("Error: {}", e); }
         }
